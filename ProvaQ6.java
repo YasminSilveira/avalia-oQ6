90494bb0939e96e1517a0537a5ba76a29ec1a647
Yasmin <64554444+YasminSilveira@users.noreply.github.com>
2020-07-10 17:13:31 -0300

    Add files via upload

---
+++ ProvaQ6.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package provaq6;
+
+/**
+ *
+ * @author yasmi
+ */
+public class ProvaQ6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+     int valor=0,pos,neg;
+
+       while(valor==0){
+       valor= Integer.parseInt(JOptionPane.showInputDialog("Informe um numero, por favor"));
+
+       if(valor>=0){
+       pos=valor*2;
+       JOptionPane.showMessageDialog(null, "Fim do calculo! ="+pos);
+       }
+       if(valor<0){
+       neg=valor*3;
+       JOptionPane.showMessageDialog(null, "Fim do calculo! ="+neg);
+       }
+}
+
+  
